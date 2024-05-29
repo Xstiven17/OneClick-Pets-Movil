cac6e75dd7cdbd0d2f0650bd473b292b6ed0752d
Xstiven17 <mzstiven17@gmail.com>
2024-05-28 19:35:42 -0500

    ZZZ

--- app/src/main/java/com/aaa/inicio11/RegVendedorFragment.kt
+++ app/src/main/java/com/aaa/inicio11/RegVendedorFragment.kt
@@ -4,18 +4,48 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.aaa.inicio11.databinding.FragmentRegVendedorBinding
 
 
 class RegVendedorFragment : Fragment() {
 
+    private var _binding: FragmentRegVendedorBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
-        return inflater.inflate(R.layout.fragment_reg_vendedor, container, false)
+        _binding = FragmentRegVendedorBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnatras.setOnClickListener {
+            activity?.onBackPressed()
+        }
+
+        binding.boton1.setOnClickListener {
+
+            val email = binding.edit1.text.toString()
+            val nit = binding.edit2.text.toString()
+            val telefono = binding.edit3.text.toString()
+            val contraseña = binding.edit4.text.toString()
+
+            if (email.isNotEmpty() && nit.isNotEmpty() && telefono.isNotEmpty() && contraseña.isNotEmpty()) {
+                Toast.makeText(context, "Datos guardados correctamente", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
+
