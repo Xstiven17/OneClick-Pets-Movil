3aec5a9825c5729fbb18e8a2b0327268f837543f
Xstiven17 <mzstiven17@gmail.com>
2024-06-01 13:19:37 -0500

    bolalo

--- app/src/main/java/com/aaa/inicio11/CarritoComprasFragment.kt
+++ app/src/main/java/com/aaa/inicio11/CarritoComprasFragment.kt
@@ -6,40 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+class CarritoComprasFragment : Fragment() {
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-
-class CarritoComprasFragment : Fragment() { // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_carrito_compras, container, false)
-    }
-
-    companion object {
-
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CarritoComprasFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-}
--- app/src/main/java/com/aaa/inicio11/MainActivity.kt
+++ app/src/main/java/com/aaa/inicio11/MainActivity.kt
@@ -10,7 +10,6 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.Theme_Inicio11)
 
         super.onCreate(savedInstanceState)
-<<<<<<< HEAD
         setContentView(R.layout.fragment_inicio_sesion)
 
         if (savedInstanceState == null) {
@@ -18,9 +17,8 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.fragment_container, InicioSesionFragment())
                 .commit()
         }
-=======
         setContentView(R.layout.fragment_config_cuenta)
->>>>>>> 456032729d1b665df0eae0b6b2e63a1ee4856bd0
+
     }
 }
 
